6b06896226e9f3ca9b24af4eeb7673c0b4cb89a5
xkeyscore <antonin.raymondaud@gmail.com>
2025-04-14 15:24:00 +0200

    Button component updated, MovieCard component updated

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -3,6 +3,7 @@ import { Icons } from "../icons";
 import styles from "./MovieCard.module.css";
 
 import genresData from "../../mocks/genres.json";
+import { Button } from "../button";
 
 interface MovieCardProps {
 	id: number;
@@ -21,24 +22,22 @@ export const MovieCard = ({
 		console.log("ADD/REM Watchlist: ", id);
 	};
 
-	/* console.log(genresData.genres); */
-
 	return (
-		<>
-			<div className={styles.card}>
-				<figure className={styles.cardFigure}>
-					<Link to={`/movie/${id}`}>
-						<img
-							className={styles.cardImg}
-							src={`https://image.tmdb.org/t/p/w780${backdrop_path}`}
-							alt={title}
-						/>
-					</Link>
-					<figcaption className={styles.cardInfosContainer}>
-						<div className={styles.cardInfos}>
-							<h2 className={styles.title}>{title}</h2>
-							<ul className={styles.genres}>
-								{genre_ids.map((genre_id, index) => {
+		<div className={styles.card} role="button" tab-index="0">
+			<figure className={styles.figure}>
+				<Link to={`/movie/${id}`}>
+					<img
+						className={styles.figureImage}
+						src={`https://image.tmdb.org/t/p/w780${backdrop_path}`}
+						alt={title}
+					/>
+				</Link>
+				<figcaption className={styles.figcaption}>
+					<div className={styles.figcaptionInfo}>
+						<h2 className={styles.title}>{title}</h2>
+						<ul className={styles.genres}>
+							{
+								genre_ids.map((genre_id, index) => {
 									const genre = genresData.genres.find(
 										(genre) => genre.id === genre_id,
 									)!;
@@ -46,29 +45,30 @@ export const MovieCard = ({
 										<li key={genre_id}>
 											<Link
 												to={`/search/genre/${genre_id}`}
-												className={styles.genre}
+												className={styles.link}
 											>
 												{genre.name}
 											</Link>
 											{index < genre_ids.length - 1 && " -"}
 										</li>
 									);
-								})}
-							</ul>
-						</div>
-						<button
-							className={styles.button}
-							type="button"
-							onClick={(e) => {
-								e.preventDefault();
-								handleWatchlist(id);
-							}}
-						>
-							<Icons.AddToList />
-						</button>
-					</figcaption>
-				</figure>
-			</div>
-		</>
+								})
+							}
+						</ul>
+					</div>
+					<Button
+						size="small"
+						variant="ghost"
+						className={styles.button}
+						onClick={(e) => {
+							e.preventDefault();
+							handleWatchlist(id);
+						}}
+					>
+						<Icons.AddToList />
+					</Button>
+				</figcaption>
+			</figure>
+		</div>
 	);
 };
