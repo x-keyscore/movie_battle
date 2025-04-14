41b3033d3215e5dd00012cf3a5595356a614fb3d
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-14 16:30:06 +0200

    ActorCard done

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,5 +1,34 @@
+import movieCredits from "./../mocks/credits-movie.json";
+import { ActorCard } from "./../components/actorCard";
+
 const MovieDetailsPage = () => {
-	return <h1>Movie Details Page</h1>;
+	return (
+		<>
+			<h1>Movie Details Page</h1>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
+					gap: "16px",
+					padding: "16px",
+				}}
+			>
+				{movieCredits.cast
+					.filter((member) => member.known_for_department === "Acting")
+					.map((actor) => {
+						return (
+							<ActorCard
+								key={actor.id}
+								id={actor.id}
+								name={actor.original_name}
+								character={actor.character}
+								profile_path={actor.profile_path}
+							/>
+						);
+					})}
+			</div>
+		</>
+	);
 };
 
 export default MovieDetailsPage;
