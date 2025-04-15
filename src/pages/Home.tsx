aaee1b009e4f5d38a8cde1589f07e80f95bbc474
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-15 12:33:52 +0200

    Save

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,9 @@
-import { MovieCard } from "../components/MovieCard";
-import moviesData from "./../mocks/movies.json";
-
-const TESTMOVIES = moviesData.results;
+import { MovieSection } from "../components/MovieSection";
 
 const HomePage = () => {
 	return (
 		<>
-			<h1>Home Page</h1>
-			<div
-				style={{
-					display: "grid",
-					placeItems: "center",
-					gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
-					gap: "16px",
-					padding: "16px",
-				}}
-			>
-				{TESTMOVIES.map((TESTMOVIE) => {
-					return (
-						<MovieCard
-							key={TESTMOVIE.id}
-							id={TESTMOVIE.id}
-							title={TESTMOVIE.title}
-							genreIds={TESTMOVIE.genre_ids}
-							backdropPath={TESTMOVIE.backdrop_path}
-						/>
-					);
-				})}
-			</div>
+			<MovieSection type="Populaire" />
 		</>
 	);
 };
