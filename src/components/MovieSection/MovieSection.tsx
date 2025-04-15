da361cceca99832e191d3e387327e6c2ef2f5d46
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-15 17:27:19 +0200

    Save

--- src/components/MovieSection/MovieSection.tsx
+++ src/components/MovieSection/MovieSection.tsx
@@ -4,25 +4,40 @@ import styles from "./MovieSection.module.css";
 
 interface MovieSectionProps {
 	type: string;
+	maxNbrCards: number;
+	oneLine: boolean;
 }
 
-export const MovieSection = ({ type }: MovieSectionProps) => {
+export const MovieSection = ({
+	type,
+	maxNbrCards,
+	oneLine,
+}: MovieSectionProps) => {
 	return (
 		<>
-			<h2 className={styles.sectionTitle}>{type}</h2>
-			<div className={styles.container}>
-				<ul className={styles.moviesList}>
-					{moviesData.results.map((movie) => (
-						<li key={movie.id} className={styles.movieItem}>
-							<MovieCard
-								id={movie.id}
-								title={movie.title}
-								genreIds={movie.genre_ids}
-								backdropPath={movie.backdrop_path}
-							/>
-						</li>
-					))}
-				</ul>
+			<div className={styles.sectionMovies}>
+				<h2 className={styles.sectionTitle}>Films - {type}</h2>
+				<div className={styles.container}>
+					<ul
+						className={oneLine ? styles.moviesListOneLine : styles.moviesList}
+					>
+						{moviesData.results
+							.filter((movie, index) => movie && index < maxNbrCards)
+							.map((movie) => (
+								<li
+									key={movie.id}
+									className={oneLine ? styles.movieItemFixedWidth : ""}
+								>
+									<MovieCard
+										id={movie.id}
+										title={movie.title}
+										genreIds={movie.genre_ids}
+										backdropPath={movie.backdrop_path}
+									/>
+								</li>
+							))}
+					</ul>
+				</div>
 			</div>
 		</>
 	);
