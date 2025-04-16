fb6b4bf9f9c90d0316500019a64341ac766c3b4f
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-16 12:31:04 +0200

    Save

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,10 +1,37 @@
+import movie from "./../mocks/movie.json";
 import movieCredits from "./../mocks/credits-movie.json";
+import { MovieSection } from "../components/MovieSection";
 import { ActorCard } from "../components/ActorCard";
+import styles from "./MovieDetails.module.css";
 
 const MovieDetailsPage = () => {
+	console.log(movie);
 	return (
-		<>
-			<h1>Movie Details Page</h1>
+		<div className={styles.movieDetails}>
+			<h1 className={styles.pageTitle}>Details</h1>
+			<ul className={styles.detailsList}>
+				<li className={styles.detailItem}>
+					<h3 className={styles.detailTitle}>Durée</h3>
+					<p className={styles.detail}>
+						{/* formatMovieLength */ movie.runtime}
+					</p>
+				</li>
+				<li className={styles.detailItem}>
+					<h3 className={styles.detailTitle}>Date de sortie</h3>
+					<p className={styles.detail}>
+						{/* formatReleaseDate */ movie.release_date}
+					</p>
+				</li>
+				<li className={styles.detailItem}>
+					<h3 className={styles.detailTitle}>Budgget</h3>
+					<p className={styles.detail}>{/* formatCost */ movie.budget}</p>
+				</li>
+				<li className={styles.detailItem}>
+					<h3 className={styles.detailTitle}>Revenue</h3>
+					<p className={styles.detail}>{/* formatCost */ movie.revenue}</p>
+				</li>
+			</ul>
+
 			<div
 				style={{
 					display: "grid",
@@ -27,7 +54,7 @@ const MovieDetailsPage = () => {
 						);
 					})}
 			</div>
-		</>
+		</div>
 	);
 };
 
