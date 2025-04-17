ff69106e8637c01d7c09537d6e77a28b9bbde933
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-17 12:30:11 +0200

    save

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -2,13 +2,14 @@ import movieCredits from "./../mocks/credits-movie.json";
 import { ActorCard } from "../components/ActorCard";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
-import type { movieDetails } from "../requests/tmdb-types/tmdbMovie";
+import type { MovieDetails } from "../requests/tmdb-types/tmdbMovie";
 import styles from "./MovieDetails.module.css";
+import { MovieSection } from "../components/MovieSection";
 
 const MovieDetailsPage = () => {
 	const { movie_id } = useParams();
 
-	const [movie, setMovie] = useState<movieDetails | null>(null);
+	const [movie, setMovie] = useState<MovieDetails | null>(null);
 
 	useEffect(() => {
 		const url = `https://api.themoviedb.org/3/movie/${movie_id}?language=fr-FR`;
@@ -22,65 +23,17 @@ const MovieDetailsPage = () => {
 
 		fetch(url, options)
 			.then((res) => res.json())
-			.then((json) => setMovie(() => json))
+			.then((json) => setMovie(json))
 			.catch((err) => console.error(err));
 	}, [movie_id]);
 
 	if (!movie) return <h1>Can't find movie</h1>;
 
 	return (
-		<div className={styles.movieDetails}>
-			<h1 className={styles.pageTitle}>Details</h1>
-			<div className={styles.container}>
-				<div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Durée :</h3>
-						<p className={styles.detail}>
-							{/* formatMovieLength */ movie.runtime}
-						</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Date de sortie :</h3>
-						<p className={styles.detail}>
-							{/* formatReleaseDate */ movie.release_date}
-						</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Budgget :</h3>
-						<p className={styles.detail}>{/* formatCost */ movie.budget}</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Revenue :</h3>
-						<p className={styles.detail}>{/* formatCost */ movie.revenue}</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Langue d'origine :</h3>
-						<p className={styles.detail}>{movie.original_language}</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Pays d'origine :</h3>
-						<p className={styles.detail}>{movie.origin_country[0]}</p>
-					</div>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Directeurs :</h3>
-						<ul className={styles.detailList}>
-							{movieCredits.cast
-								.filter((member) => member.known_for_department === "Directing")
-								.map((director) => {
-									return <li key={director.id}>{director.original_name}</li>;
-								})}
-						</ul>
-					</div>
-				</div>
-				<div className={styles.column}>
-					<div className={styles.detailItem}>
-						<h3 className={styles.detailTitle}>Maisons de production :</h3>
-						<ul className={styles.detailList}>
-							{movie.production_companies.map((company) => {
-								return <li key={company.id}>{company.name}</li>;
-							})}
-						</ul>
-					</div>
+		<>
+			<MovieSection type="Similaire" maxNbrCards={10} oneLine={true} />
+			<div className={styles.detailsSection}>
+				<figure className={styles.detailsContainer}>
 					<div className={styles.imgContainer}>
 						<img
 							src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
@@ -88,9 +41,58 @@ const MovieDetailsPage = () => {
 							className={styles.img}
 						/>
 					</div>
-				</div>
+					<figcaption className={styles.details}>
+						<ul>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Durée :</h3>
+								<p className={styles.detail}>{movie.runtime}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Date de sortie :</h3>
+								<p className={styles.detail}>{movie.release_date}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Budgget :</h3>
+								<p className={styles.detail}>{movie.budget}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Revenue :</h3>
+								<p className={styles.detail}>{movie.revenue}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Langue d'origine :</h3>
+								<p className={styles.detail}>{movie.original_language}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Pays d'origine :</h3>
+								<p className={styles.detail}>{movie.origin_country[0]}</p>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Directeurs :</h3>
+								<ul className={styles.detailList}>
+									{movieCredits.cast
+										.filter(
+											(member) => member.known_for_department === "Directing",
+										)
+										.map((director) => {
+											return (
+												<li key={director.id}>{director.original_name}</li>
+											);
+										})}
+								</ul>
+							</li>
+							<li className={styles.detailItem}>
+								<h3 className={styles.detailTitle}>Maisons de production :</h3>
+								<ul className={styles.detailList}>
+									{movie.production_companies.map((company) => {
+										return <li key={company.id}>{company.name}</li>;
+									})}
+								</ul>
+							</li>
+						</ul>
+					</figcaption>
+				</figure>
 			</div>
-
 			<div
 				style={{
 					display: "grid",
@@ -113,7 +115,7 @@ const MovieDetailsPage = () => {
 						);
 					})}
 			</div>
-		</div>
+		</>
 	);
 };
 
