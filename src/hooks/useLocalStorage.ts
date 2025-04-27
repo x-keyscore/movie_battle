4ea4e684ee117a82006e0010d58b8c468ec2aa27
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-27 17:07:28 +0200

    Formated details, small correctionsÃ

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,20 +3,19 @@ import { useEffect, useState } from "react";
 const PREFIX = import.meta.env.VITE_APP_PREFIX;
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
-    const prefixedKey: string = PREFIX + "-" + key;
-    const [value, setValue] = useState<T>(() => {
-        const jsonValue = localStorage.getItem(prefixedKey);
-        if (jsonValue != null) return JSON.parse(jsonValue);
-        if (typeof initialValue === "function") {
-            return (initialValue as () => T)();
-        } else {
-            return initialValue;
-        }
-    });
+	const prefixedKey: string = PREFIX + "-" + key;
+	const [value, setValue] = useState<T>(() => {
+		const jsonValue = localStorage.getItem(prefixedKey);
+		if (jsonValue != null) return JSON.parse(jsonValue);
+		if (typeof initialValue === "function") {
+			return (initialValue as () => T)();
+		}
+		return initialValue;
+	});
 
-    useEffect(() => {
-        if (value != null) localStorage.setItem(prefixedKey, JSON.stringify(value))
-    }, [prefixedKey, value])
+	useEffect(() => {
+		if (value != null) localStorage.setItem(prefixedKey, JSON.stringify(value));
+	}, [prefixedKey, value]);
 
-    return [value, setValue] as const;
+	return [value, setValue] as const;
 }
