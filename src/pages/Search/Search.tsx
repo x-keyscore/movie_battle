279b1d133e9a4d2a1cc1cf6f213e2d674054230e
xkeyscore <antonin.raymondaud@gmail.com>
2025-04-30 14:02:16 +0200

    save

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,42 +1,73 @@
-import type { Movie } from "../../api";
+import type { Movie, MovieList } from "../../api";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { useApp } from "../../providers/AppProvider";
+import { useRequest } from "../../hooks/useRequest";
 import { MovieSectionReveal } from "../../components";
 import { requests } from "../../api";
 
-type FetchMovies = Parameters<typeof MovieSectionReveal>[0]['fetchMovies'];
+interface UseRequestData {
+	movies: Movie[];
+	maxPages: number;
+}
 
 export function SearchPage() {
 	const { movieTitle } = useParams();
+	const { setTopmovie } = useApp();
 	const [pageIndex, setPageIndex] = useState(1);
+
+	const [data] = useRequest<UseRequestData>(
+		{ movies: [], maxPages: 1 },
+		async (getPrevData) => {
+			const prevData = getPrevData();
+			let data: null | MovieList = null;
+
+			if (pageIndex > prevData.maxPages) return (prevData);
+
+			if (movieTitle) {
+				data = (await requests.movie.getMoviesByTitle({
+					page: pageIndex,
+					query: movieTitle,
+					language: "fr-FR"
+				})).data;
+			}
+
+			if (!data) {
+				return ({ 
+					maxPages: prevData.maxPages,
+					movies: [...prevData.movies]
+				});
+			}
+			else if (pageIndex === 1) {
+				return ({
+					maxPages: data.total_pages,
+					movies: data.results.slice(1)
+				});
+			}
+			else {
+				return ({
+					maxPages: prevData.maxPages,
+					movies: [...prevData.movies, ...data.results]
+				});
+			}
+		},
+		[pageIndex, movieTitle]
+	);
+
+	useEffect(() => {
+		if (data.movies[0]) setTopmovie(data.movies[0]);
+	}, [data, setTopmovie]);
 
 	useEffect(() => {
 		setPageIndex(1);
 	}, [movieTitle]);
 
-	const fetchMovies: FetchMovies = async (prevMovies) => {
-		let movies: null | Movie[] = [];
-
-		if (movieTitle) {
-			movies = (await requests.movie.getMoviesByTitle({
-				query: movieTitle,
-				page: pageIndex,
-				language: "fr-FR"
-			})).data.results;
-		}
-
-		if (pageIndex === 1) return (movies.slice(1));
-		else return ([...prevMovies(), ...movies]);
-	}
-
 	return (
 		<>
 			<MovieSectionReveal
 				inline={false}
-				pageIndex={pageIndex}
-				setPageIndex={setPageIndex}
-				fetchMovies={fetchMovies}
-				fetchDependencies={[pageIndex, movieTitle]}
+				movies={data.movies}
+				onScrollEnd={() => setPageIndex(prev => prev + 1)}
 			/>
 		</>
 	);
