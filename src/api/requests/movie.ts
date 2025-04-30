0c9a8a40bdbfe16700c8addfa66a1388b01fe202
Burdon Jordan <burdon.jordan@gmail.com>
2025-04-30 08:28:55 +0200

    discover movie fetch for quizz

--- src/api/requests/movie.ts
+++ src/api/requests/movie.ts
@@ -63,3 +63,32 @@ interface GetSelectedGenreParams {
 
 export const getSelectedGenre = (params: GetSelectedGenreParams) =>
 	tmdb.get<MovieList>("/discover/movie", { params });
+
+interface GetDiscoverMovieParams {
+	language?: string;
+	page?: number;
+	primary_release_year?: number; // 2006 - List of movies from that year
+	"primary_release_date.gte"?: string; // 2006-01-01 - LOM released at that date and later
+	"primary_release_date.lte"?: string; // 2006-12-31 - LOM released at that date and before
+	sort_by?:
+		| "popularity.desc"
+		| "revenue.desc"
+		| "vote_count.desc"
+		| "vote_average.desc"; // Defaults to popularity
+	"vote_average.gte"?: number; // Between 0 and 10 - LOM at this rating or greater
+	"vote_average.lte"?: number; // Between 0 and 10 - LOM at this rating or lesser
+	"vote_count.gte"?: number; // LOM with this number of votes or greater
+	with_people?: string; // String of cast/crew IDs - Can be used with and "," or "|" - LOM with people in it
+	with_cast?: string; // Same but only for cast
+	with_crew?: string; // Same but only for crew
+	with_companies?: string; // String of company IDs
+	without_companies?: string; // String of company IDs to exclude
+	with_genres?: string; // String of genre IDs
+	without_genres?: string;
+	with_keywords?: string; // String of keywords IDs
+	with_origin_country?: string; // FR - Use iso3166 - LOM from origin country
+	with_origin_language?: string; // fr - Use iso639 - LOM with origin language
+}
+
+export const getDiscoverMovie = (params: GetDiscoverMovieParams) =>
+	tmdb.get<MovieList>("/discover/movie", { params });
